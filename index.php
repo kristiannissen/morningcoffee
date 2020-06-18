9831a0cf0f100628c1c4554a1db6ae5c3d695609
Kristian Nissen <kristian.nissen@gmail.com>
2020-06-18 19:52:17 +0200

    Working on context

--- index.php
+++ index.php
@@ -5,4 +5,12 @@ include_once 'vendor/autoload.php';
 use MorningCoffee\Coffee;
 
 $my_coffee = new Coffee();
-print_r($my_coffee);
+
+$my_template_file = "./test_file.html";
+// Define a key/value object as template context
+$context = [
+    'name' => 'Kitty',
+    'greeting' => 'Hello'
+];
+
+echo $my_coffee->render($my_template_file, $context);
--- src/MorningCoffee/Coffee.php
+++ src/MorningCoffee/Coffee.php
@@ -3,8 +3,37 @@
 namespace MorningCoffee;
 
 class Coffee {
+    protected $file_path;
+    protected $file_content;
+
     public function __construct()
     {
-        return "Hello Kitty";
+        $this->file_path = "";
+        $this->file_content = "";
+    }
+    /*
+     * @param $file_path string
+     * @param context array
+     * @return string
+     */
+    public function render(string $file_path, array $context)
+    {
+        $this->file_path = $file_path;
+
+        if (!file_exists($file_path))
+            return user_error();
+
+        $this->file_content = file_get_contents($this->file_path);
+
+        if (count($context) > 0)
+        {
+            $keys = array_map(function($key) {
+                return $key;
+            }, array_keys($context));
+
+            var_dump($keys);
+
+            // $this->file_content = str_replace();
+        }
     }
 }
