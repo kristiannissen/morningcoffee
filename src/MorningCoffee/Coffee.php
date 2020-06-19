826e518497cf1f32a43d291e50f6530cea58378e
Kristian Nissen <kristian.nissen@gmail.com>
2020-06-19 16:43:00 +0200

    Bit of refactoring

--- src/MorningCoffee/Coffee.php
+++ src/MorningCoffee/Coffee.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace MorningCoffee;
-
+/**
+ * class Coffee
+ */
 class Coffee {
     protected $file_path;
     protected $file_content;
@@ -12,33 +14,52 @@ class Coffee {
         $this->file_content = "";
     }
     /*
-     * @param $file_path string
-     * @param context array
+     * @param string $file_path
+     * @param array $context
      * @return string
      */
-    public function render(string $file_path, array $context)
+    public function render(string $file_path, array $context = [])
     {
         $this->file_path = $file_path;
+        $markup = "";
 
         if (!file_exists($file_path))
-            return user_error();
+            //TODO: Should throw an exception instead
+            return trigger_error(
+                "The file $file_path does not exist",
+                E_USER_ERROR
+            );
 
         $this->file_content = file_get_contents($this->file_path);
 
+        if ($this->file_content == "")
+            // TODO: Should throw an exception instead
+            return trigger_error(
+                "The file has no content",
+                E_USER_ERROR
+            );
+
         if (count($context) > 0)
         {
-            $key_val = [];
-            foreach ($context as $key => $val)
-            {
-                $key_val["{".$key."}"] = $val;
-            }
-            $markup = str_replace(
-                array_keys($key_val),
-                array_values($key_val),
-                $this->file_content
-            );
-            
-            return $markup;
+           $this->file_content = $this->parseContextArray($context); 
         }
+
+        return $this->file_content;
+    }
+
+    public function parseContextArray(array $context)
+    {
+        $key_val = [];
+        foreach ($context as $key => $val)
+        {
+            $key_val["{".$key."}"] = $val;
+        }
+        $markup = str_replace(
+            array_keys($key_val),
+            array_values($key_val),
+            $this->file_content
+        );
+
+        return $markup;
     }
 }
