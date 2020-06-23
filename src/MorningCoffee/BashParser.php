3f109ab3fa79ebda464fb52b930a3f2a794c502e
Kristian <kristian.nissen@gmail.com>
2020-06-23 09:42:10 +0200

    Spotted some typos

--- src/MorningCoffee/BashParser.php
+++ src/MorningCoffee/BashParser.php
@@ -19,118 +19,123 @@ class BashParser implements ParserInterface
      */
     public function parse(string $content)
     {
-      $line = strtok($content, PHP_EOL);
+        $line = strtok($content, PHP_EOL);
 
-      while ($line !== false) {
-        if (Str::contains($line, '/([^el]if)\s?\[{2}/')) {
-          // if [[ 1 + 1 = 2]] then
-          $code = str_replace(
-            ["[[", "]]", "=", " then"],
-            ["(", ")", "==", ":"],
-            trim($line)
-          );
-          $match = Str::matches($line, '/([^el]if)\s?\[{2}/');
-          $content = substr_replace(
-            $content,
-            "<?php ". $code ." ?>",
-            stripos($content, $match),
-            strlen(trim($line))
-          );
+        while ($line !== false) {
+            if (Str::contains($line, '/([^el]if)\s?\[{2}/')) {
+                // if [[ 1 + 1 = 2]] then
+                $code = str_replace(
+                    ["[[", "]]", "=", " then"],
+                    ["(", ")", "==", ":"],
+                    trim($line)
+                );
+                $match = Str::matches($line, '/([^el]if)\s?\[{2}/');
+                $content = substr_replace(
+                    $content,
+                    "<?php " . $code . " ?>",
+                    stripos($content, $match),
+                    strlen(trim($line))
+                );
+            }
+
+            if (Str::contains($line, "/(elif)\s?\[{2}/")) {
+                // elif [[ 1 + 1 = 3]] then
+                $code = str_replace(
+                    ["elif", "[[", "]]", "=", " then"],
+                    ["elseif", "(", ")", "==", ""],
+                    trim($line)
+                );
+                $match = Str::matches(
+                    $line,
+                    '/(elif)\s?\[{2}(.*)\]{2}\s?(then)/'
+                );
+                $content = substr_replace(
+                    $content,
+                    "<?php " . $code . ": ?>",
+                    stripos($content, $match),
+                    strlen(trim($line))
+                );
+            }
+
+            if (Str::contains($line, '/(fi)/')) {
+                // fi
+                $match = Str::matches($line, '/(fi)/');
+                $content = substr_replace(
+                    $content,
+                    "<?php endif ?>",
+                    stripos($content, $match),
+                    strlen(trim($line))
+                );
+            }
+
+            if (Str::contains($line, '/(echo)/')) {
+                // echo "1 + 1 is 2"
+                $match = Str::matches($line, '/(echo)\s=?(.*)/');
+                $content = substr_replace(
+                    $content,
+                    "<?php " . trim($match) . " ?>",
+                    stripos($content, $match),
+                    strlen(trim($line))
+                );
+            }
+
+            if (Str::contains($line, '/([A-Z]{1}\w+)=(.*)$/')) {
+                // Variables
+                $match = Str::matches($line, '/([A-Z]{1}\w+)=(.*)$/');
+                preg_match('/\((.*)\)/', trim($match), $vars);
+                $vars_arr = str_replace(
+                    [$vars[1], "(", ")"],
+                    [join(',', explode(' ', $vars[1])), "[", "]"],
+                    $match
+                );
+                $content = substr_replace(
+                    $content,
+                    "<?php $" . trim($vars_arr) . "; ?>",
+                    stripos($content, $match),
+                    strlen(trim($line))
+                );
+            }
+
+            if (
+                Str::contains($line, '/(for)\s([A-Z]\w+)\s(as)\s(\w+)\s(do)$/')
+            ) {
+                // for loop
+                preg_match(
+                    '/(for)\s([A-Z]\w+)\s(as)\s(\w+)\s(do)$/',
+                    trim($line),
+                    $matches
+                );
+
+                $code = $matches[0];
+                $code = str_replace(
+                    ["for", "do", $matches[2], $matches[4]],
+                    ["foreach (", ")", "$" . $matches[2], "$" . $matches[4]],
+                    $code
+                );
+                $content = substr_replace(
+                    $content,
+                    "<?php " . trim($code) . ": ?>",
+                    stripos($content, $matches[0]),
+                    strlen(trim($line))
+                );
+            }
+
+            if (Str::is($line, "done")) {
+                $content = substr_replace(
+                    $content,
+                    "<?php endforeach ?>",
+                    stripos($content, "done"),
+                    strlen(trim($line))
+                );
+            }
+
+            $line = strtok(PHP_EOL);
         }
 
-        if (Str::contains($line, "/(elif)\s?\[{2}/")) {
-          // elif [[ 1 + 1 = 3]] then
-          $code = str_replace(
-            ["elif", "[[", "]]", " then"],
-            ["elseif", "(", ")", ":"],
-            trim($line)
-          );
-          $match = Str::matches($line, '/(elif)\s?\[{2}(.*)\]{2}\s?(then)/');
-          $content = substr_replace(
-            $content,
-            "<?php ". $code ." ?>",
-            stripos($content, $match),
-            strlen(trim($line))
-          );
-        }
-
-        if (Str::contains($line, '/(fi)/')) {
-          // fi
-          $match = Str::matches($line, '/(fi)/');
-          $content = substr_replace(
-            $content,
-            "<? endif ?>",
-            stripos($content, $match),
-            strlen(trim($line))
-          );
-        }
-
-        if (Str::contains($line, '/(echo)/')) {
-          // echo "1 + 1 is 2"
-          $match = Str::matches($line, '/(echo)\s=?(.*)/');
-          $content = substr_replace(
-            $content,
-            "<?php ". trim($match) ." ?>",
-            stripos($content, $match),
-            strlen(trim($line))
-          );
-        }
-
-        if (Str::contains($line, '/([A-Z]{1}\w+)=(.*)$/')) {
-          // Variables
-          $match = Str::matches($line, '/([A-Z]{1}\w+)=(.*)$/');
-          preg_match('/\((.*)\)/', trim($match), $vars);
-          $vars_arr = str_replace(
-            $vars[1],
-            join(',', explode(' ', $vars[1])),
-            $match
-          );
-          $content = substr_replace(
-            $content,
-            "<?php $". trim($vars_arr) ."; ?>",
-            stripos($content, $match),
-            strlen(trim($line))
-          );
-        }
-
-        if (Str::contains($line, '/(for)\s([A-Z]\w+)\s(as)\s(\w+)\s(do)$/')) {
-          // for loop
-          preg_match(
-            '/(for)\s([A-Z]\w+)\s(as)\s(\w+)\s(do)$/',
-            trim($line),
-            $matches
-          );
-
-          $code = $matches[0];
-          $code = str_replace(
-            ["for", "do", $matches[2],  $matches[4]],
-            ["foreach (", ")", "$". $matches[2], "$". $matches[4]],
-            $code
-          );
-          $content = substr_replace(
-            $content,
-            "<?php ". trim($code) .": ?>",
-            stripos($content, $matches[0]),
-            strlen(trim($line))
-          );
-        }
-
-        if (Str::is($line, "done")) {
-          $content = substr_replace(
-            $content,
-            "<?php endforeach ?>",
-            stripos($content, "done"),
-            strlen(trim($line))
-          );
-        }
-
-        $line = strtok(PHP_EOL);
-      }
-
-      return $content;
+        return $content;
     }
 
-    public static function parseIf($content) {
-      
+    public static function parseIf($content)
+    {
     }
 }
--- src/MorningCoffee/Str.php
+++ src/MorningCoffee/Str.php
@@ -2,34 +2,40 @@
 
 namespace MorningCoffee;
 
-class Str {
-  /**
-   * @param string $string
-   * @param string $chars
-   * */
-  public static function starts_with($str, $chars) : string {
-    return (substr($str, 0, strlen($str)) === $chars);
-  }
-
-  public static function ends_with($str, $chars) : string {
-    $len = strlen($str);
-    if ($len == 0) {
-      return true;
+class Str
+{
+    /**
+     * @param string $string
+     * @param string $chars
+     * */
+    public static function starts_with($str, $chars): string
+    {
+        return substr($str, 0, strlen($str)) === $chars;
     }
-    return (substr($str, - $len) === $chars);
-  }
 
-  public static function contains($str, $reg) : bool {
-    preg_match($reg, $str, $m);
-    return count($m);
-  }
+    public static function ends_with($str, $chars): string
+    {
+        $len = strlen($str);
+        if ($len == 0) {
+            return true;
+        }
+        return substr($str, -$len) === $chars;
+    }
 
-  public static function matches($str, $reg) : string {
-    preg_match($reg, $str, $m);
-    return trim($m[0]);
-  }
+    public static function contains($str, $reg): bool
+    {
+        preg_match($reg, $str, $m);
+        return count($m);
+    }
 
-  public static function is($a, $b) : bool {
-    return trim($a) === trim($b);
-  }
+    public static function matches($str, $reg): string
+    {
+        preg_match($reg, $str, $m);
+        return trim($m[0]);
+    }
+
+    public static function is($a, $b): bool
+    {
+        return trim($a) === trim($b);
+    }
 }
